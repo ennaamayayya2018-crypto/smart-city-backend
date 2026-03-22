ac287325a1c389945bb63b164532210294858d3d
Ina <ennaamayayya2018@gmail.com>
2026-03-22 17:47:50 +0100

    إزالة حارس Multer من مسار الرخص لتوافق طلبات JSON وتأكيد الإصدار

--- routes/licenceRoutes.js
+++ routes/licenceRoutes.js
@@ -2,17 +2,13 @@ const express = require('express');
 const router = express.Router();
 const licenceController = require('../controllers/licenceController');
 
-// استدعاء الحارس لفحص وثيقة الرخصة
-const uploadMiddleware = require('../middlewares/uploadMiddleware');
-
 // مسار جلب الملفات
 router.get('/demandes', licenceController.getDemandesLicence);
 
 // مسار البحث
 router.get('/recherche', licenceController.rechercherDemande);
 
-// مسار إصدار الرخصة (هنا نستخدم الحارس) 🚨
-// الحارس يبحث عن ملف واحد اسمه 'document_autorisation'
-router.put('/delivrer', uploadMiddleware, licenceController.delivrerLicence);
+// 🚀 التعديل الذهبي: إزالة حارس (uploadMiddleware) للسماح بمرور طلب الـ JSON بسلاسة
+router.put('/delivrer', licenceController.delivrerLicence);
 
 module.exports = router;
